b65fbf235997e3a5cd75feed676050bae93129a8
Anvar <anv.sult@gmail.com>
2025-05-23 11:04:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/smart_beauty_salon/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.smart_beauty_salon.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
